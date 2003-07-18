b16ad6927a25d28b7602cdf4821bf492001f2029
aleix <devnull@localhost>
2003-07-18 09:01:24 +0200

    minor changes in documentation

--- src/element.h
+++ src/element.h
@@ -84,9 +84,11 @@ scew_element_next(scew_element const* parent, scew_element const* element);
  * Returns the child element on the specified position. Positions are
  * zero based.
  *
- * Do not call this function if you just want to iterate through all the
- * elements, you will have a serious performance degradation. Use
- * <code>scew_element_next</code> instead.
+ * Do not call this function if you just want to iterate through the
+ * elements, because you would have a serious performance degradation in
+ * large documents. Use <code>scew_element_next</code> instead.
+ *
+ * This function is for element random access.
  *
  * @see scew_element_next
  *
