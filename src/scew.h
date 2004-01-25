df82b13c6d57d04c919fb7ce5dfbe1582aa301ab
aleix <devnull@localhost>
2004-01-26 00:05:02 +0100

    added writer

--- src/scew.h
+++ src/scew.h
@@ -23,7 +23,7 @@
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
  *
  * @endif
  */
@@ -39,5 +39,6 @@
 #include "parser.h"
 #include "str.h"
 #include "tree.h"
+#include "writer.h"
 
 #endif /* SCEW_H_ALEIX0211250134 */
