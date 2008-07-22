1796a7d837e43c5d9960404d932ef4efe81b34cb
Aleix Conchillo Flaqué <aconchillo@ieec.cat>
2008-07-22 23:06:36 +0200

    remove carriage returns from autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -3,15 +3,15 @@ rm -f ltconfig ltmain.sh config.cache aclocal.m4
 # remove the autoconf cache
 rm -rf autom4te*.cache
 set -e
-echo -n "libtoolize... "
+echo "libtoolize... "
 ${LIBTOOLIZE:-libtoolize} --copy --force --automake >/dev/null
-echo -n "aclocal... "
+echo "aclocal... "
 ${ACLOCAL:-aclocal} -I etc/autoconf
-echo -n "autoheader... "
+echo "autoheader... "
 ${AUTOHEADER:-autoheader}
-echo -n "autoconf... "
+echo "autoconf... "
 ${AUTOCONF:-autoconf}
-echo -n "automake... "
+echo "automake... "
 ${AUTOMAKE:-automake} -a
 echo okay.
 # remove the autoconf cache
