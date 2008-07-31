a5d2d8d67d3af11d2b185adb1d2e3c0457a32e5b
Aleix Conchillo Flaqué <aleix@member.fsf.org>
2008-07-31 22:34:54 +0200

    add xwriter.h copyright notice

--- scew/xwriter.h
+++ scew/xwriter.h
@@ -1,8 +1,29 @@
 /**
- * @file
- * @author   Aleix Conchillo Flaque <aconchillo@ieec.cat>
+ * @file     xwriter.h
+ * @brief    Declaration of internal SCEW writer type
+ * @author   Aleix Conchillo Flaque <aleix@member.fsf.org>
  * @date     Tue Jul 22, 2008 22:05
- * @brief    SCEW
+ *
+ * @if copyright
+ *
+ * Copyright (C) 2008 Aleix Conchillo Flaque
+ *
+ * SCEW is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * SCEW is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ *
+ * @endif
  **/
 
 #ifndef XWRITER_H_0807222205
@@ -21,14 +42,15 @@ enum
 
 // Types
 
-typedef bool (*scew_writer_close_hook) (scew_writer *);
+typedef bool (*scew_writer_close_callback) (scew_writer *);
 
-typedef bool (*scew_writer_printf_hook) (scew_writer *, XML_Char const *, ...);
+typedef bool (*scew_writer_printf_callback) (scew_writer *,
+                                             XML_Char const *, ...);
 
 struct scew_writer
 {
-  scew_writer_close_hook close;
-  scew_writer_printf_hook printf;
+  scew_writer_close_callback close;
+  scew_writer_printf_callback printf;
   bool indented;
   unsigned int indent;
   unsigned int spaces;
