8344c02692204d06b255dd28194279c30d06e5d9
Aleix Conchillo Flaque <aleix@member.fsf.org>
2009-10-30 17:03:23 +0100

    automatically generate ChangeLog file

--- autogen.sh
+++ autogen.sh
@@ -3,6 +3,11 @@ rm -f ltconfig ltmain.sh config.cache aclocal.m4
 # remove the autoconf cache
 rm -rf autom4te*.cache
 set -e
+
+echo "changelog..."
+(hg log -r tip:170 --style changelog > ChangeLog) || {
+    echo "Error generating ChangeLog. The file won't be up to date."
+}
 echo "libtoolize... "
 ${LIBTOOLIZE:-libtoolize} --copy --force --automake >/dev/null
 echo "aclocal... "
